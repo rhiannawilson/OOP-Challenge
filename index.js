d67ae2d9fe6db4802013d5e394d29835d187a5ef
rmay <159880101+rhiannawilson@users.noreply.github.com>
2024-05-31 17:33:38 +1000

    starter code

---
+++ index.js
@@ -0,0 +1,30 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { circle, triangle, square } = require('./lib/shapes.js');
+
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "Text : Enter up to three characters:"
+
+    },
+    {
+        type: "input",
+        name: "text-color",
+        message: "Text Color: Enter a color keyword (OR a hexadecimal number"
+
+    },
+    {
+        type: "input",
+        name: "shape-color",
+        message: "Shape Color: Enter a color keyword (OR a hexadecimal number"
+    },
+    {
+        type: "list",
+        name: "image-size",
+        message: "Choose which pixel mage you would like?",
+        choices: ["circle, triangle, square"]
+    },
+];
+// console.log(questions);
